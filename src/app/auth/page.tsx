03217bfaeb1b94ebd5631616751ed5d2755bb086
Davi Silva <davi.pimenta8@gmail.com>
2024-12-19 20:36:20 -0300

    feat: add logo and adjust layout for login page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,32 +8,36 @@ import {
 import { Button } from "@/components/ui/button";
 import { authWithGoogle } from "@/app/_actions/auth";
 import { GoogleIcon } from "./_components/google-icon";
+import Image from "next/image";
 
 const AuthPage = () => {
   return (
-    <form
-      action={authWithGoogle}
-      className="min-h-screen flex items-center justify-center bg-gray-100"
-    >
-      <Card className="w-full max-w-md">
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl font-bold text-center">
-            Login
-          </CardTitle>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <Button variant="outline" className="w-full">
-            <GoogleIcon className="mr-2 h-4 w-4" />
-            Login com o Google
-          </Button>
-        </CardContent>
-        <CardFooter>
-          <p className="text-sm text-center text-gray-600 mt-4 w-full">
-            Ao fazer login, você concorda com os nossos termos de privacidade.
-          </p>
-        </CardFooter>
-      </Card>
-    </form>
+    <>
+      <Image src="/logo.svg" width={116} height={34} alt="Logo da Stockly" />
+      <form
+        action={authWithGoogle}
+        className="h-full flex flex-col items-center justify-center"
+      >
+        <Card className="w-full max-w-md">
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl font-bold text-center">
+              Login
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <Button variant="outline" className="w-full">
+              <GoogleIcon className="mr-2 h-4 w-4" />
+              Login com o Google
+            </Button>
+          </CardContent>
+          <CardFooter>
+            <p className="text-sm text-center text-gray-600 mt-4 w-full">
+              Ao fazer login, você concorda com os nossos termos de privacidade.
+            </p>
+          </CardFooter>
+        </Card>
+      </form>
+    </>
   );
 };
 
