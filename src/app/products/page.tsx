fc7243be07037886fb5ea99831dc5729eb0622e2
Davi Silva <davi.pimenta8@gmail.com>
2024-12-19 21:04:19 -0300

    feat: create initial products page with titles and user-specific table

---
+++ src/app/products/page.tsx
@@ -0,0 +1,17 @@
+import { DataTable } from "@/components/ui/data-table";
+import { TitleWithSubtitle } from "../_components/title-with-subtitle";
+import { getProducts } from "./_data-access/get-products";
+import { productTableColumns } from "./_components/table-columns";
+
+const ProductsPage = async () => {
+  const products = await getProducts();
+
+  return (
+    <div>
+      <TitleWithSubtitle title="Gestão de Produtos" subtitle="Produtos" />
+      <DataTable data={products!} columns={productTableColumns} />
+    </div>
+  );
+};
+
+export default ProductsPage;
