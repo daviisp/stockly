af731279de35a90c831441b0f1b755ffd7193c8b
Davi Silva <davi.pimenta8@gmail.com>
2024-12-19 21:02:08 -0300

    feat: add getProducts function for fetching user-specific products

---
+++ src/app/products/_data-access/get-products.ts
@@ -0,0 +1,18 @@
+import "server-only";
+
+import { prisma } from "@/lib/prisma";
+import { auth } from "@/services/auth";
+
+export const getProducts = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return;
+  }
+
+  return await prisma.product.findMany({
+    where: {
+      userId: session?.user?.id as string,
+    },
+  });
+};
