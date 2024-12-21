632c30fac000d62829de12f9c165c887e36ea218
Davi Silva <davi.pimenta8@gmail.com>
2024-12-21 14:42:35 -0300

    style: make title font size responsive for better readability

--- src/app/_components/title-with-subtitle.tsx
+++ src/app/_components/title-with-subtitle.tsx
@@ -10,7 +10,7 @@ export const TitleWithSubtitle = ({
   return (
     <div className="flex flex-col gap-2 mb-5">
       <span className="text-[#00A180] text-xs font-semibold">{subtitle}</span>
-      <h2 className="text-xl font-semibold">{title}</h2>
+      <h2 className="text-base sm:text-xl font-semibold">{title}</h2>
     </div>
   );
 };
