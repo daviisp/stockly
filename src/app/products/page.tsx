6597d636e382ba0a7d70728f1e189aa725796dfa
Davi Silva <davi.pimenta8@gmail.com>
2024-12-21 14:44:00 -0300

    feat: update products page layout with create product button

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,13 +2,17 @@ import { DataTable } from "@/components/ui/data-table";
 import { TitleWithSubtitle } from "../_components/title-with-subtitle";
 import { getProducts } from "./_data-access/get-products";
 import { productTableColumns } from "./_components/table-columns";
+import { CreateProduct } from "./_components/create-product";
 
 const ProductsPage = async () => {
   const products = await getProducts();
 
   return (
-    <div>
-      <TitleWithSubtitle title="Gestão de Produtos" subtitle="Produtos" />
+    <div className="bg-white h-full p-5 rounded-md">
+      <div className="flex justify-between items-center pb-8 sm:pb-0">
+        <TitleWithSubtitle title="Gestão de Produtos" subtitle="Produtos" />
+        <CreateProduct />
+      </div>
       <DataTable data={products} columns={productTableColumns} />
     </div>
   );
