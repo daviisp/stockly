088355345128e33ec7570a730610051954ca1398
Davi Silva <davi.pimenta8@gmail.com>
2024-12-28 17:32:42 -0300

    chore: remove unused imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,18 @@
-import {
-  CircleDollarSign,
-  DollarSignIcon,
-  PackageIcon,
-  ShoppingBasket,
-} from "lucide-react";
 import { TitleWithSubtitle } from "../_components/title-with-subtitle";
-import { StatsSummaryCard } from "./_components/stats-summary-card.tsx";
-import { RevenueSummaryCard } from "./_components/revenue-summary-card";
+import { getPopularProducts } from "./_data-access/get-popular-products";
+import { TotalRevenueCard } from "./_components/total-revenue-card";
+import { SummarySkeletonCard } from "./_components/summary-skeleton-card";
+import { TotalRevenueTodayCard } from "./_components/total-revenue-today-card";
+import { TotalSalesQuantityCard } from "./_components/total-sales-quantity-card";
+import { TotalInStockCard } from "./_components/total-in-stock-card";
+import { TotalProductsQuantityCard } from "./_components/total-products-quantity-card";
+import { Last14DaysRevenueCard } from "./_components/last-14-days-revenue-card";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 import { PopularProducts } from "./_components/popular-products";
-import { getDashboard } from "./_data-access/get-dashboard";
 
 const DashboardPage = async () => {
-  const {
-    totalSales,
-    totalSalesToday,
-    totalSalesQuantity,
-    totalStock,
-    totalProducts,
-  } = await getDashboard();
+  const data = await getPopularProducts();
 
   return (
     <div>
@@ -25,46 +20,55 @@ const DashboardPage = async () => {
         <TitleWithSubtitle subtitle="Dashbord" title="Dashboard" />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-4">
-        <RevenueSummaryCard
-          icon={<DollarSignIcon />}
-          text="Receita total"
-          value={totalSales}
-        />
-        <RevenueSummaryCard
-          icon={<DollarSignIcon />}
-          text="Receita hoje"
-          value={totalSalesToday}
-        />
+        <Suspense fallback={<SummarySkeletonCard />}>
+          <TotalRevenueCard />
+        </Suspense>
+        <Suspense fallback={<SummarySkeletonCard />}>
+          <TotalRevenueTodayCard />
+        </Suspense>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
-        <StatsSummaryCard
-          icon={<CircleDollarSign />}
-          text="Vendas totais"
-          value={totalSalesQuantity}
-        />
-        <StatsSummaryCard
-          icon={<PackageIcon />}
-          text="Total em estoque"
-          value={totalStock}
-        />
-        <StatsSummaryCard
-          icon={<ShoppingBasket />}
-          text="Produtos"
-          value={totalProducts}
-        />
+        <Suspense fallback={<SummarySkeletonCard />}>
+          <TotalSalesQuantityCard />
+        </Suspense>
+        <Suspense fallback={<SummarySkeletonCard />}>
+          <TotalInStockCard />
+        </Suspense>
+        <Suspense fallback={<SummarySkeletonCard />}>
+          <TotalProductsQuantityCard />
+        </Suspense>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-        <div>Gráfico</div>
-        <div className="p-8 rounded-lg bg-white">
-          <p className="text-slate-900 text-lg font-semibold pb-8">
-            Produtos mais vendidos
-          </p>
-          <div className="flex flex-col gap-8">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <PopularProducts key={index} />
-            ))}
-          </div>
-        </div>
+      <div className="grid grid-cols-1 xl:grid-cols-[2fr,1fr] gap-4 mt-4 mb-5 max-h-fit">
+        <Suspense
+          fallback={
+            <Skeleton className="bg-white w-full rounded-xl p-6">
+              <Skeleton className="bg-gray-200 rounded-xl w-[58.93px] h-7" />
+              <Skeleton className="bg-gray-200 rounded-xl w-[94.81px] h-6 mt-1" />
+              <Skeleton className="bg-gray-200 rounded-xl w-full h-full mt-3" />
+            </Skeleton>
+          }
+        >
+          <Last14DaysRevenueCard />
+        </Suspense>
+        <Suspense
+          fallback={
+            <Skeleton className="w-full bg-white rounded-xl p-8">
+              <Skeleton className="bg-gray-200 rounded-xl w-[195.7px] h-7 mb-8" />
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-3">
+                  <Skeleton className="bg-gray-200 rounded-xl p-1.5 w-[80.79px] h-5" />
+                  <div>
+                    <Skeleton className="bg-gray-200 rounded-xl w-[75px] h-6" />
+                    <Skeleton className="bg-gray-200 rounded-xl w-[70px] h-6 mt-1" />
+                  </div>
+                </div>
+                <Skeleton className="bg-gray-200 rounded-xl w-5 h-6" />
+              </div>
+            </Skeleton>
+          }
+        >
+          <PopularProducts />
+        </Suspense>
       </div>
     </div>
   );
