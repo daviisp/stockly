9c9646292d97a49c7253ed7737d32e61e0fc4ac4
Davi Silva <davi.pimenta8@gmail.com>
2024-12-28 17:32:00 -0300

    style: adjust border

--- src/app/dashboard/_components/last-14-days-revenue-card.tsx
+++ src/app/dashboard/_components/last-14-days-revenue-card.tsx
@@ -5,7 +5,7 @@ export const Last14DaysRevenueCard = async () => {
   const last14DaysRevenue = await getLast14DaysRevenue();
 
   return (
-    <div className="bg-white rounded-lg p-6">
+    <div className="bg-white rounded-xl p-6">
       <p className="text-slate-900 font-semibold text-lg">Receita</p>
       <p className="pt-1 text-slate-400 text-sm mb-3">Últimos 14 dias</p>
       <Graph data={last14DaysRevenue} />
